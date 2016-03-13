3846b5aca4d6844353e4b13cdf64f779329b2c8d
Chloe Weber <chloeesierraa@yahoo.com>
2016-03-12 21:50:01 -0600

    Add listener HTML and JS event listener

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var heading = document.getElementById( 'headlineOne' );
+
+heading.addEventListener( 'click', function() {
+    var myElement = document.createElement( 'article' );
+    myElement.innerHTML = '<p>This is click number x.</p>';
+    document.getElementsByClassName( 'container' )[ 0 ].appendChild( myElement );
+} ) ;
