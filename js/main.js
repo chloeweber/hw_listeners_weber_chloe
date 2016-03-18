0fd7e68a258e7598da10a6bf6c4dac86d27bf3d5
Chloe Weber <chloeesierraa@yahoo.com>
2016-03-18 00:26:43 -0500

    Finish counting section of listener

--- js/main.js
+++ js/main.js
@@ -2,6 +2,17 @@ var heading = document.getElementById( 'headlineOne' );
 
 heading.addEventListener( 'click', function() {
     var myElement = document.createElement( 'article' );
-    myElement.innerHTML = '<p>This is click number x.</p>';
+    myElement.innerHTML = '<p>This is click number</p>';
     document.getElementsByClassName( 'container' )[ 0 ].appendChild( myElement );
 } ) ;
+
+var add = ( function() {
+    var counter = 0;
+    return function() {return counter += 1;};
+} )();
+
+document.getElementById( 'headlineOne' ).addEventListener( 'click', countingFunction );
+
+function countingFunction() {
+    document.getElementById( 'headlineOne' ).innerHTML = add();
+}
