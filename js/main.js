be15ccd75b74ab259a655d54d8a9449866beb4ad
Chloe Weber <chloeesierraa@yahoo.com>
2016-03-17 19:41:55 -0500

    Finalize index and JS code and edit CSS

--- js/main.js
+++ js/main.js
@@ -1,9 +1,32 @@
-var textInput = document.getElementsByClassName( 'info' );
-
+var firstNameInput = document.getElementById( 'firstName' );
 document.querySelector( 'form.simpleForm' ).addEventListener( 'submit', function( event ) {
 
-    //prevent the normal submission of the form
     event.preventDefault();
 
-    console.log( textInput.value );
+    console.log( 'The first name is: ' + firstNameInput.value );
+} );
+
+var lastNameInput = document.getElementById( 'lastName' );
+document.querySelector( 'form.simpleForm' ).addEventListener( 'submit', function( event ) {
+
+    event.preventDefault();
+
+    console.log( 'The last name is: ' + lastNameInput.value );
+} );
+
+var emailInput = document.getElementById( 'email' );
+document.querySelector( 'form.simpleForm' ).addEventListener( 'submit', function( event ) {
+
+    event.preventDefault();
+
+    console.log( 'The email is: ' + emailInput.value );
+} );
+
+var messageInput = document.getElementById( 'message' );
+document.querySelector( 'form.simpleForm' ).addEventListener( 'submit', function( event ) {
+
+    event.preventDefault();
+
+    console.log( 'The message is: ' + messageInput.value );
+    console.log( 'Wow this is a cool tool!' );
 } );
