bffb06339252a82a531c5a1d248c8d876001eae1
Chloe Weber <chloeesierraa@yahoo.com>
2016-03-18 00:05:05 -0500

    Add third custom event listener

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-/* Event Listener 1: This event listener listens for the click on h2 with the id:
+/* Event Listener 1: This event listener listens for a double click on the h2 with the id:
 headlineTwo. Once heard, this listener appends the section with the class: mainContent
 with an article that holds a paragraph with information on Corgi temperements.*/
 
 var heading = document.getElementById( 'headlineTwo' );
 
-heading.addEventListener( 'click', function() {
+heading.addEventListener( 'dblclick', function() {
     var myElement = document.createElement( 'article' );
     myElement.innerHTML = '<p>Corgis are very active and energetic. They have a strong desire to please and should receive both physical and mental exercise regularly. They should be socialized early on because they tend to be shy and cautious with strangers and other dogs. They have a tendency to be very vocal, and for this reason make good alarm dogs.</p>';
     document.getElementsByClassName( 'mainContent' )[ 0 ].appendChild( myElement );
@@ -24,4 +24,20 @@ function changeImage() {
         } else {
         image.src = 'img/corgibutt.jpg';
         }
+}
+
+/* Event Listener 3: This event listener listens for the mouse to click down on the h1
+and for the mouse to be released. Once heard, this listener changes the color of the
+h1 from black (original color) to white (when mouse is down) and finally blue
+(when mouse is up).*/
+
+document.getElementById( 'headlineOne' ).addEventListener( 'mousedown', mouseDown );
+document.getElementById( 'headlineOne' ).addEventListener( 'mouseup', mouseUp );
+
+function mouseDown () {
+    document.getElementById( 'headlineOne' ).style.color = 'white';
+}
+
+function mouseUp () {
+    document.getElementById( 'headlineOne' ).style.color = 'blue';
 }
