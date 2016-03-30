f132e924111d946ca3f6db1a11c4f180eb96b751
Richard Kalehoff <richardkalehoff@gmail.com>
2016-03-29 21:47:43 -0700

    increase the counter with each click

--- js/main.js
+++ js/main.js
@@ -5,4 +5,5 @@ heading.addEventListener( 'click', function() {
     var myElement = document.createElement( 'article' );
     myElement.innerHTML = '<p>This is click number ' + counter + '</p>';
     document.getElementsByClassName( 'container' )[ 0 ].appendChild( myElement );
+    counter = counter + 1;
 } ) ;
