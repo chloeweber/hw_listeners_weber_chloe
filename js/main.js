6cde35bed1cf01cd9abc8b708c52cc5e7f7a84a6
Richard Kalehoff <richardkalehoff@gmail.com>
2016-03-29 21:46:45 -0700

    create a counter

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var heading = document.getElementById( 'headlineOne' );
+var counter = 1;
 
 heading.addEventListener( 'click', function() {
     var myElement = document.createElement( 'article' );
-    myElement.innerHTML = '<p>This is click number</p>';
+    myElement.innerHTML = '<p>This is click number ' + counter + '</p>';
     document.getElementsByClassName( 'container' )[ 0 ].appendChild( myElement );
 } ) ;
