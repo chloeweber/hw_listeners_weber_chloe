bd33f0880a47c95919eabfe9819365d01d82fc06
Richard Kalehoff <richardkalehoff@gmail.com>
2016-03-29 21:46:03 -0700

    remove unneeded code

--- js/main.js
+++ js/main.js
@@ -5,14 +5,3 @@ heading.addEventListener( 'click', function() {
     myElement.innerHTML = '<p>This is click number</p>';
     document.getElementsByClassName( 'container' )[ 0 ].appendChild( myElement );
 } ) ;
-
-var add = ( function() {
-    var counter = 0;
-    return function() {return counter += 1;};
-} )();
-
-document.getElementById( 'headlineOne' ).addEventListener( 'click', countingFunction );
-
-function countingFunction() {
-    document.getElementById( 'headlineOne' ).innerHTML = add();
-}
